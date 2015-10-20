e098fb96bf63e05542200383f4bb66d656373430
Charles Hill <chill@degreesofzero.com>
2015-10-20 21:38:24 +0200

    Added tests for getProxies and getProxiesFromSource

--- test/unit/getProxies.js
+++ test/unit/getProxies.js
@@ -6,4 +6,29 @@ describe('getProxies([options, ]cb)', function() {
 
 		expect(ProxyLists.getProxies).to.be.a('function');
 	});
+
+	it('should call getProxies() for all sources', function() {
+
+		var testSources = ['somewhere', 'somewhere-else'];
+		var called = [];
+
+		_.each(testSources, function(name) {
+			ProxyLists.addSource(name, {
+				getProxies: function() {
+					called.push(name);
+				}
+			});
+		});
+
+		ProxyLists.getProxies();
+
+		_.each(testSources, function(name) {
+			expect(_.contains(called, name)).to.equal(true);
+		});
+
+		// Clean-up.
+		_.each(testSources, function(name) {
+			delete ProxyLists._sources[name];
+		});
+	});
 });
--- test/unit/getProxiesFromSource.js
+++ test/unit/getProxiesFromSource.js
@@ -6,4 +6,41 @@ describe('getProxiesFromSource(name, [options, ]cb)', function() {
 
 		expect(ProxyLists.getProxiesFromSource).to.be.a('function');
 	});
+
+	it('should throw an error if the source does not exist', function() {
+
+		var name = 'does-not-exist';
+		var thrownError;
+
+		try {
+
+			ProxyLists.getProxiesFromSource(name, function() { });
+
+		} catch (error) {
+
+			thrownError = error;
+		}
+
+		expect(thrownError).to.not.equal(undefined);
+		expect(thrownError instanceof Error).to.equal(true);
+		expect(thrownError.message).to.equal('Proxy source does not exist: "' + name + '"');
+	});
+
+	it('should call getProxies() method of the specified source', function() {
+
+		var name = 'somewhere';
+		var called = false;
+
+		ProxyLists.addSource(name, {
+			getProxies: function() {
+				called = true;
+			}
+		});
+
+		ProxyLists.getProxiesFromSource(name);
+		expect(called).to.equal(true);
+
+		// Clean-up.
+		delete ProxyLists._sources[name];
+	});
 });
