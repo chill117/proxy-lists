69a55d14855aa61efedae997b8194b356ec49450
Charles Hill <chill@degreesofzero.com>
2016-04-10 16:20:24 +0200

    Refactored source.getProxies tests.

--- test/unit/sources/index.js
+++ test/unit/sources/index.js
@@ -5,33 +5,13 @@ var expect = require('chai').expect;
 
 var ProxyLists = require('../../../index');
 
-var listSourcesOptions = {
-	sourcesBlackList: []
-};
+describe('source.getProxies([options, ]cb)', function() {
 
-if (process.env.TRAVIS_CI) {
-	listSourcesOptions.sourcesBlackList.push('freeproxylist');
-}
+	_.each(_.keys(ProxyLists._sources), function(name) {
 
-if (!process.env.PROXY_LISTS_BITPROXIES_API_KEY) {
-	listSourcesOptions.sourcesBlackList.push('bitproxies');
-}
+		var source = ProxyLists._sources[name];
 
-if (!process.env.PROXY_LISTS_KINGPROXIES_API_KEY) {
-	listSourcesOptions.sourcesBlackList.push('kingproxies');
-}
-
-var sources = ProxyLists.listSources(listSourcesOptions);
-
-_.each(sources, function(source) {
-
-	var name = source.name;
-
-	source = ProxyLists._sources[name];
-
-	describe('source.' + name, function() {
-
-		describe('getProxies([options, ]cb)', function() {
+		describe('source.' + name, function() {
 
 			it('should be a function', function() {
 
@@ -40,39 +20,59 @@ _.each(sources, function(source) {
 
 			it('should return valid proxies', function(done) {
 
-				this.timeout(30000);
+				if (process.env.TRAVIS_CI) {
+					console.log('Skipping this test because it doesn\'t run well on travis-ci platform.');
+					return this.skip();
+				}
 
-				var gotProxies = false;
-				var cb = _.once(done);
-
-				var options = {
-					anonymityLevels: ['anonymous', 'elite', 'transparent'],
-					protocols: ['http', 'https', 'socks4', 'socks5'],
-					sample: true
-				};
+				var options = {};
 
 				switch (name) {
 
 					case 'bitproxies':
-						if (process.env.PROXY_LISTS_BITPROXIES_API_KEY) {
-							options.bitproxies = {};
-							options.bitproxies.apiKey = process.env.PROXY_LISTS_BITPROXIES_API_KEY;
+						if (!process.env.PROXY_LISTS_BITPROXIES_API_KEY) {
+							console.log('Skipping this test because bitproxies API key was not found.');
+							return this.skip();
 						}
+						options.bitproxies = {};
+						options.bitproxies.apiKey = process.env.PROXY_LISTS_BITPROXIES_API_KEY;
 					break;
 
 					case 'kingproxies':
-						if (process.env.PROXY_LISTS_KINGPROXIES_API_KEY) {
-							options.kingproxies = {};
-							options.kingproxies.apiKey = process.env.PROXY_LISTS_KINGPROXIES_API_KEY;
+						if (!process.env.PROXY_LISTS_KINGPROXIES_API_KEY) {
+							console.log('Skipping this test because kingproxies API key was not found.');
+							return this.skip();
 						}
+						options.kingproxies = {};
+						options.kingproxies.apiKey = process.env.PROXY_LISTS_KINGPROXIES_API_KEY;
 					break;
 				}
+
+				this.timeout(30000);
+
+				var gotProxies = false;
+				var cb = _.once(function(error) {
+
+					if (error) {
+						return done(error);
+					}
+
+					if (!gotProxies) {
+						return done(new Error('Expected to get some proxies.'));
+					}
+
+					done();
+				});
+
+				options = _.extend(options, {
+					anonymityLevels: ['anonymous', 'elite', 'transparent'],
+					protocols: ['http', 'https', 'socks4', 'socks5'],
+					sample: true
+				});
 
 				options = ProxyLists.prepareOptions(options);
 
 				var gettingProxies = source.getProxies(options);
-
-				gettingProxies.on('error', cb);
 
 				gettingProxies.on('data', function(proxies) {
 
@@ -83,16 +83,16 @@ _.each(sources, function(source) {
 					try {
 
 						expect(proxies).to.be.an('array');
-						expect(proxies.length > 0).to.equal(true);
+
+						if (!(proxies.length > 0)) {
+							throw new Error('Expected at least one proxy.');
+						}
 
 						_.each(proxies, function(proxy) {
 
 							try {
-
 								expect(ProxyLists.isValidProxy(proxy)).to.equal(true);
-
 							} catch (error) {
-
 								invalidProxies.push(proxy);
 							}
 						});
@@ -104,18 +104,8 @@ _.each(sources, function(source) {
 					}
 				});
 
-				gettingProxies.on('end', function() {
-
-					try {
-
-						expect(gotProxies).to.equal(true);
-
-					} catch (error) {
-						return cb(error);
-					}
-
-					cb();
-				});
+				gettingProxies.on('error', cb);
+				gettingProxies.on('end', cb);
 			});
 		});
 	});
