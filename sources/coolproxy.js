b0997ddfb8753f75d848f0d4ad5700fca165bcb7
Charles Hill <chill@degreesofzero.com>
2017-07-09 14:44:26 +0200

    Minor performance improvements, style changes

--- sources/coolproxy.js
+++ sources/coolproxy.js
@@ -1,108 +1,109 @@
-var EventEmitter = require("events");
-var async = require("async");
-var request = require("request");
-var _ = require("underscore");
-var cheerio = require("cheerio");
+var EventEmitter = require('events');
+var async = require('async');
+var request = require('request');
+var _ = require('underscore');
+var cheerio = require('cheerio');
+
+var decodeProxyRegex = /str_rot13\("(.+)"\)/;
 
 module.exports = {
-	homeUrl: "http://www.cool-proxy.net/proxies/http_proxy_list",
 
-	getProxies: function () {
+	homeUrl: 'http://www.cool-proxy.net/proxies/http_proxy_list',
+
+	getProxies: function() {
 		var emitter = new EventEmitter();
-		async.seq(
-			_.bind(this.getPageLinks, this),
-			_.bind(this.parsePages, this, emitter)
-		)(function () {
-			emitter.emit("end");
+		var getProxiesFromPage = async.seq(
+			_.bind(this.getPage, this),
+			_.bind(this.parsePage, this)
+		);
+		this.getPageLinks(function(error, links) {
+			async.each(links, function(link, next) {
+				getProxiesFromPage(link, function(error, proxies) {
+					if (error) {
+						emitter.emit('error', error);
+					} else {
+						emitter.emit('data', proxies);
+					}
+					next();
+				});
+			}, function(){
+				emitter.emit('end');
+			});
 		});
-
 		return emitter;
 	},
 
-	getPageLinks: function (cb) {
-		var url = this.homeUrl;
+	getPageLinks: function(cb) {
+		var homeUrl = this.homeUrl;
 		request({
-			method: "GET",
-			url: this.homeUrl,
-		}, function (err, res, body) {
-			if (err) {
-				return cb(err);
+			method: 'GET',
+			url: homeUrl,
+		}, function(error, res, body) {
+			if (error) {
+				return cb(error);
 			}
-			var $ = cheerio.load(body);
-			$("span.next").remove();
-			var maxPage = $(".pagination a").last().html();
-			var links = [];
-			for (var i = 1; i <= maxPage; i++) {
-				links.push(url + "/page:" + i);
+			try {
+				var $ = cheerio.load(body);
+				$('span.next').remove();
+				var maxPage = $('.pagination a').last().html();
+				var links = [];
+				for (var index = 1; index <= maxPage; index++) {
+					links.push(homeUrl + '/page:' + index);
+				}
+			} catch (error) {
+				return cb(error);
 			}
-			return cb(null, links);
+			cb(null, links);
 		});
 	},
 
-	parsePages: function (emitter, links, cb) {
-		var totalPages = links.length;
-		_.each(links, _.bind(function (link) {
-			async.seq(
-				this.getPage,
-				_.bind(this.parsePage, this)
-			)(link, function (err, proxies) {
-				--totalPages;
-
-				if (err) {
-					emitter.emit("error", err);
-				}
-
-				if (proxies) {
-					emitter.emit("data", proxies);
-				}
-
-				if (totalPages === 0) {
-					cb();
-				}
-			});
-		}, this));
-	},
-
-	getPage: function (link, cb) {
+	getPage: function(link, cb) {
 		request({
-			method: "GET",
+			method: 'GET',
 			url: link,
-		}, function (err, res, html) {
-			if (err) {
-				return cb(err);
+		}, function(error, res, html) {
+			if (error) {
+				return cb(error);
 			}
 			return cb(null, html);
 		});
 	},
 
-	parsePage: function (html, cb) {
-		var $ = cheerio.load(html);
-		$("table tr").last().remove();
-		var proxies = $("table tr").map(_.bind(function (i, tr) {
-			var tds = $(tr).find("td");
-			if (tds.length < 3) {
-				return null;
+	parsePage: function(html, cb) {
+		var decodeProxy = _.bind(this.decodeProxy, this);
+		_.defer(function() {
+			try {
+				var $ = cheerio.load(html);
+				$('table tr').last().remove();
+				var proxies = $('table tr').map(function(index, tr) {
+					var tds = $(tr).find('td');
+					if (tds.length < 3) {
+						return null;
+					}
+					var ip = decodeProxy($(tds[0]).html());
+					var port = $(tds[1]).html();
+					var anonymity = $(tds[5]).html() === 'Anonymous' ? 'anonymous' : 'transparent';
+					return {
+						ipAddress: ip,
+						port: parseInt(port),
+						protocols: ['http'],
+						anonymityLevel: anonymity,
+					};
+				}).get();
+			} catch (error) {
+				return cb(error);
 			}
-			var ip = this.decodeProxy($(tds[0]).html());
-			var port = $(tds[1]).html();
-			var anonymity = $(tds[5]).html() === "Anonymous" ? "anonymous" : "transparent";
-			return {
-				ipAddress: ip,
-				port: parseInt(port),
-				protocols: ["http"],
-				anonymityLevel: anonymity,
-			};
-		}, this)).get();
-		return cb(null, proxies);
+			cb(null, proxies);
+		});
 	},
 
-	decodeProxy: function (html) {
-		var strRot13 = function (str) {
-			return (str + "").replace(/[a-z]/gi, function (s) {
-				return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < "n" ? 13 : -13));
+	decodeProxy: function(html) {
+		var strRot13 = function(str) {
+			return (str + '').replace(/[a-z]/gi, function(s) {
+				return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < 'n' ? 13 : -13));
 			});
 		};
-		var hash = html.match(/str_rot13\("(.+)"\)/);
-		return Buffer.from(strRot13(hash[1]), "base64").toString();
-	},
+		var hash = html.match(decodeProxyRegex);
+		return Buffer.from(strRot13(hash[1]), 'base64').toString();
+	}
 };
--- test/unit/sources/coolproxy.js
+++ test/unit/sources/coolproxy.js
@@ -3,7 +3,6 @@
 var expect = require('chai').expect;
 var _ = require('underscore');
 
-
 describe('source.coolproxy', function() {
 
 	var coolproxy = require('../../../sources/coolproxy');
@@ -27,9 +26,13 @@ describe('source.coolproxy', function() {
 		});
 
 		it('should return page links', function(done) {
-			coolproxy.getPageLinks(function(err, links) {
-				expect(err).to.equal(null);
-				expect(links).to.be.an('array');
+			coolproxy.getPageLinks(function(error, links) {
+				try {
+					expect(error).to.equal(null);
+					expect(links).to.be.an('array');
+				} catch (error) {
+					return done(error);
+				}
 				done();
 			});
 		});
@@ -43,18 +46,22 @@ describe('source.coolproxy', function() {
 
 		it('should parse and return proxies', function(done) {
 			var sample = require('../../samples/coolproxy/listHtml');
-			coolproxy.parsePage(sample.toString(), function(err, proxies) {
-				expect(err).to.equal(null);
-				expect(proxies).to.be.an('array');
-				expect(proxies.length > 0).to.equal(true);
-				_.each(proxies, function(proxy) {
-					expect(proxy.ipAddress).to.not.equal(undefined);
-					expect(proxy.ipAddress).to.be.a('string');
-					expect(proxy.port).to.not.equal(undefined);
-					expect(proxy.port).to.be.a('number');
-					expect(proxy.port).to.equal(parseInt(proxy.port));
-					expect(proxy.protocols).to.be.an('array');
-				});
+			coolproxy.parsePage(sample.toString(), function(error, proxies) {
+				try {
+					expect(error).to.equal(null);
+					expect(proxies).to.be.an('array');
+					expect(proxies.length > 0).to.equal(true);
+					_.each(proxies, function(proxy) {
+						expect(proxy.ipAddress).to.not.equal(undefined);
+						expect(proxy.ipAddress).to.be.a('string');
+						expect(proxy.port).to.not.equal(undefined);
+						expect(proxy.port).to.be.a('number');
+						expect(proxy.port).to.equal(parseInt(proxy.port));
+						expect(proxy.protocols).to.be.an('array');
+					});
+				} catch (error) {
+					return done(error);
+				}
 				done();
 			});
 		});
