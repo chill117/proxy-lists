00361cd779973ade8cc226cf42feb07066ddc5ce
pepzwee <kristjan.kirpu@gmail.com>
2018-09-09 21:11:02 +0300

    Fixes proxy-list-org source

--- sources/proxy-list-org.js
+++ sources/proxy-list-org.js
@@ -18,16 +18,39 @@ module.exports = {
 				protocols: 'li.https',
 				anonymityLevel: 'li.type',
 			},
-			nextLink: '.pagination li:nth-child(2) a',
+			nextLink: '.pagination li:nth-child(2) a, .table-menu a.next',
 		},
 		parseAttributes: {
-			ipAddress: '([0-9\.]+):[0-9]+$',
+			ipAddress: function(str) {
+				var match = str.match(/([0-9\.]+):[0-9]+$/);
+				if (match && match[1]) {
+					return match[1];
+				}
+				var base64 = str.split("'");
+				if (base64 && base64.length === 3) {
+					var addr = Buffer.from(base64[1], 'base64').toString('utf8');
+					match = addr.match(/([0-9\.]+):[0-9]+$/);
+					if (match && match[1]) {
+						return match[1];
+					}
+				}
+				return null;
+			},
 			port: function(port) {
 				var match = port.match(/.+:([0-9]+)$/);
-				if (!match || !match[1]) return null;
-				port = parseInt(match[1]);
-				if (_.isNaN(port)) return null;
-				return port;
+				if (match && match[1]) {
+					port = parseInt(match[1]);
+					if (!_.isNaN(port)) return port
+				}
+				var base64 = str.split("'");
+				if (base64 && base64.length === 3) {
+					var addr = Buffer.from(base64[1], 'base64').toString('utf8');
+					match = addr.match(/.+:([0-9]+)$/);
+					if (match && match[1]) {
+						return match[1];
+					}
+				}
+				return null
 			},
 			protocols: function(protocols) {
 				return [protocols.trim().toLowerCase()];
