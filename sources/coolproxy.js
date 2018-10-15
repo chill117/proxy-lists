93582c0d042bfc2e5f5394f8b45a8aa063444d93
Charles Hill <chill@degreesofzero.com>
2018-10-15 12:16:15 +0200

    Coolproxy fix-up, use scraper abstract

--- sources/coolproxy.js
+++ sources/coolproxy.js
@@ -1,129 +1,44 @@
 'use strict';
 
 var _ = require('underscore');
-var async = require('async');
-var cheerio = require('cheerio');
-var EventEmitter = require('events').EventEmitter || require('events');
 
-var decodeProxyRegex = /str_rot13\("(.+)"\)/;
+var convert = {
+	anonymityLevels: {
+		'Anonymous': 'anonymous',
+		'No': 'transparent',
+	},
+};
 
 module.exports = {
-
-	homeUrl: 'http://www.cool-proxy.net/proxies/http_proxy_list',
-
-	getProxies: function(options) {
-
-		options || (options = {});
-
-		var emitter = new EventEmitter();
-
-		var getProxiesFromPage = async.seq(
-			_.bind(this.getPage, this),
-			_.bind(this.parsePage, this)
-		);
-
-		this.getPageLinks(options, function(error, links) {
-			async.each(links, function(link, next) {
-				getProxiesFromPage(link, options, function(error, proxies) {
-					if (error) {
-						emitter.emit('error', error);
-					} else {
-						emitter.emit('data', proxies);
-					}
-					next();
-				});
-			}, function(){
-				emitter.emit('end');
-			});
-		});
-
-		return emitter;
+	homeUrl: 'https://www.cool-proxy.net/',
+	defaultOptions: {
+		numPagesToScrape: 10,
 	},
-
-	getPageLinks: function(options, cb) {
-
-		var homeUrl = this.homeUrl;
-
-		options.request({
-			method: 'GET',
-			url: homeUrl
-		}, function(error, res, body) {
-
-			if (error) {
-				return cb(error);
-			}
-
-			try {
-				var $ = cheerio.load(body);
-				$('span.next').remove();
-				var maxPage = $('.pagination a').last().html();
-				var links = [];
-				for (var index = 1; index <= maxPage; index++) {
-					links.push(homeUrl + '/page:' + index);
-				}
-			} catch (error) {
-				return cb(error);
-			}
-
-			cb(null, links);
-		});
+	abstract: 'scraper-paginated-list',
+	config: {
+		startPageUrl: 'https://www.cool-proxy.net/proxies/http_proxy_list',
+		selectors: {
+			item: '#main table tbody tr',
+			itemAttributes: {
+				ipAddress: 'td:nth-child(1)',
+				port: 'td:nth-child(2)',
+				anonymityLevel: 'td:nth-child(6)',
+			},
+			nextLink: '#main table .pagination span.current + span a',
+		},
+		parseAttributes: {
+			ipAddress: function(ipAddress) {
+				var match = ipAddress.match(/([^\)]+)$/);
+				return match && match[1] || null;
+			},
+			port: function(port) {
+				port = parseInt(port);
+				if (_.isNaN(port)) return null;
+				return port;
+			},
+			anonymityLevel: function(anonymityLevel) {
+				return anonymityLevel && convert.anonymityLevels[anonymityLevel.trim()] || null;
+			},
+		},
 	},
-
-	getPage: function(link, options, cb) {
-
-		options.request({
-			method: 'GET',
-			url: link
-		}, function(error, res, html) {
-
-			if (error) {
-				return cb(error);
-			}
-
-			cb(null, html);
-		});
-	},
-
-	parsePage: function(html, cb) {
-
-		var decodeProxy = _.bind(this.decodeProxy, this);
-
-		_.defer(function() {
-			try {
-				var $ = cheerio.load(html);
-				$('table tr').last().remove();
-				var proxies = $('table tr').map(function(index, tr) {
-					var tds = $(tr).find('td');
-					if (tds.length < 3) {
-						return null;
-					}
-					var ip = decodeProxy($(tds[0]).html());
-					if (!ip) {
-						return null;
-					}
-					var port = $(tds[1]).html();
-					var anonymity = $(tds[5]).html() === 'Anonymous' ? 'anonymous' : 'transparent';
-					return {
-						ipAddress: ip,
-						port: parseInt(port),
-						protocols: ['http'],
-						anonymityLevel: anonymity,
-					};
-				}).get();
-			} catch (error) {
-				return cb(error);
-			}
-			cb(null, proxies);
-		});
-	},
-
-	decodeProxy: function(html) {
-		var strRot13 = function(str) {
-			return (str + '').replace(/[a-z]/gi, function(s) {
-				return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < 'n' ? 13 : -13));
-			});
-		};
-		var hash = html.match(decodeProxyRegex);
-		return hash && Buffer.from(strRot13(hash[1]), 'base64').toString() || null;
-	}
 };
