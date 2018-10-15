f7d65a4b02c5d0c3fb85944f90a797aedac56547
Charles Hill <chill@degreesofzero.com>
2018-10-15 13:20:09 +0200

    proxy-list-org: anonLevel, protocols

--- sources/proxy-list-org.js
+++ sources/proxy-list-org.js
@@ -2,6 +2,31 @@
 
 var _ = require('underscore');
 
+var convert = {
+	anonymityLevels: {
+		'elite': 'elite',
+		'anonymous': 'anonymous',
+		'transparent': 'transparent',
+	},
+	protocols: {
+		'https': ['https'],
+		'-': ['http'],
+	},
+};
+
+var parseProxyText = function(text) {
+	if (!text) return null;
+	var parts = text.split('\'');
+	if (!parts || parts.length !== 3) return null;
+	var addr = Buffer.from(parts[1], 'base64').toString('utf8');
+	var match = addr.match(/([0-9\.]+):([0-9]+)$/);
+	if (!match || !match[1] || !match[2]) return null;
+	return {
+		ipAddress: match[1],
+		port: match[2],
+	};
+};
+
 module.exports = {
 	homeUrl: 'https://proxy-list.org/',
 	defaultOptions: {
@@ -21,42 +46,24 @@ module.exports = {
 			nextLink: '.pagination li:nth-child(2) a, .table-menu a.next',
 		},
 		parseAttributes: {
-			ipAddress: function(str) {
-				var match = str.match(/([0-9\.]+):[0-9]+$/);
-				if (match && match[1]) {
-					return match[1];
-				}
-				var base64 = str.split("'");
-				if (base64 && base64.length === 3) {
-					var addr = Buffer.from(base64[1], 'base64').toString('utf8');
-					match = addr.match(/([0-9\.]+):[0-9]+$/);
-					if (match && match[1]) {
-						return match[1];
-					}
-				}
-				return null;
+			anonymityLevel: function(anonymityLevel) {
+				anonymityLevel = anonymityLevel && anonymityLevel.trim().toLowerCase() || null;
+				return anonymityLevel && convert.anonymityLevels[anonymityLevel] || null;
+			},
+			ipAddress: function(ipAddress) {
+				var addr = parseProxyText(ipAddress);
+				return addr && addr.ipAddress || null;
 			},
 			port: function(port) {
-				var match = port.match(/.+:([0-9]+)$/);
-				if (match && match[1]) {
-					port = parseInt(match[1]);
-					if (!_.isNaN(port)) return port
-				}
-				var base64 = str.split("'");
-				if (base64 && base64.length === 3) {
-					var addr = Buffer.from(base64[1], 'base64').toString('utf8');
-					match = addr.match(/.+:([0-9]+)$/);
-					if (match && match[1]) {
-						return match[1];
-					}
-				}
-				return null
+				var addr = parseProxyText(port);
+				if (!addr || !addr.port) return null;
+				port = parseInt(addr.port);
+				if (!port || _.isNaN(port)) return null;
+				return port;
 			},
 			protocols: function(protocols) {
-				return [protocols.trim().toLowerCase()];
-			},
-			anonymityLevel: function(anonymityLevel) {
-				return anonymityLevel.trim().toLowerCase();
+				protocols = protocols && protocols.trim().toLowerCase() || null;
+				return protocols && convert.protocols[protocols] || [];
 			},
 		},
 	},
