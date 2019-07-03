7a97b08928d685794f6dcf321ab66566cdfb169b
Charles Hill <chill@degreesofzero.com>
2019-07-03 16:38:04 +0200

    Fixed xroxy

--- sources/xroxy.js
+++ sources/xroxy.js
@@ -3,62 +3,39 @@
 var _ = require('underscore');
 
 var anonymityLevels = {
-	'anonymous': 'anonymous',
-	'distorting': 'elite',
-	'socks4': 'anonymous',
-	'socks5': 'anonymous',
-	'transparent': 'transparent',
+	'Anonymous': 'anonymous',
+	'Socks4': 'anonymous',
+	'Socks5': 'anonymous',
+	'Distorting': 'elite',
+	'Transparent': 'transparent',
 };
 
 module.exports = {
 	homeUrl: 'https://www.xroxy.com/',
-	abstract: 'xml',
+	defaultOptions: {
+		numPagesToScrape: 10,
+	},
+	abstract: 'scraper-paginated-list',
 	config: {
-		feeds: [
-			{
-				requestOptions: {
-					url: 'https://[2001:19f0:200:2eda:6c3d:287b:0:1]/proxyrss.xml',
-					agentOptions: {
-						rejectUnauthorized: false,
-					},
-					headers: {
-						'Accept': 'application/xml',
-						'Host': 'www.xroxy.com',
-						'User-Agent': 'Mozilla/5.0 Chrome/70.0.3000.60',
-					}
-				},
-				paths: {
-					group: 'rss/channel/0/item',
-					item: 'prx:proxy',
-					attributes: {
-						ipAddress: 'prx:ip/0',
-						port: 'prx:port/0',
-						protocols: 'prx:ssl/0',
-						anonymityLevel: 'prx:type/0',
-					},
-				},
-				parseAttributes: {
-					port: function(port) {
-						port = parseInt(port);
-						if (_.isNaN(port)) return null;
-						return port;
-					},
-					protocols: function(ssl) {
-						var anonymityLevel = this.anonymityLevel && this.anonymityLevel.trim().toLowerCase();
-						if (anonymityLevel) {
-							switch (anonymityLevel) {
-								case 'socks4':
-								case 'socks5':
-									return [anonymityLevel];
-							}
-						}
-						return [ssl === 'true' ? 'https' : 'http'];
-					},
-					anonymityLevel: function(anonymityLevel) {
-						return anonymityLevels[anonymityLevel.trim().toLowerCase()] || null;
-					},
-				},
-			}
-		],
+		startPageUrl: 'https://www.xroxy.com/free-proxy-lists/',
+		selectors: {
+			item: '#DataTables_Table_0 > tbody > tr',
+			itemAttributes: {
+				ipAddress: 'td:nth-child(1)',
+				port: 'td:nth-child(2)',
+				anonymityLevel: 'td:nth-child(3)',
+			},
+			nextLink: '#DataTables_Table_0_paginate > ul > li.paginate_button.active + li.paginate_button a',
+		},
+		parseAttributes: {
+			port: function(port) {
+				port = parseInt(port);
+				if (_.isNaN(port)) return null;
+				return port;
+			},
+			anonymityLevel: function(anonymityLevel) {
+				return anonymityLevels[anonymityLevel.trim()] || null;
+			},
+		},
 	},
 };
