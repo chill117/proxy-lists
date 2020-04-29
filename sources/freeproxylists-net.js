ee28855b0cb69918a492446eee9b01584a4c8dc1
Charles Hill <chill@degreesofzero.com>
2020-04-29 14:23:05 +0200

    freeproxylists-net: tabs instead of spaces

--- sources/freeproxylists-net.js
+++ sources/freeproxylists-net.js
@@ -3,79 +3,79 @@
 var _ = require('underscore');
 
 var convert = {
-  anonymityLevels: {
-    'high anonymous': 'elite',
-    anonymous: 'anonymous',
-    none: 'transparent',
-  },
-  protocols: {
-    http: 'http',
-    https: 'https',
-    socks4: 'socks4',
-    socks5: 'socks5',
-  },
+	anonymityLevels: {
+		'high anonymous': 'elite',
+		anonymous: 'anonymous',
+		none: 'transparent',
+	},
+	protocols: {
+		http: 'http',
+		https: 'https',
+		socks4: 'socks4',
+		socks5: 'socks5',
+	},
 };
 
 module.exports = {
-  homeUrl: 'http://freeproxylists.net/',
-  abstract: 'list-crawler',
-  defaultOptions: {
-    defaultTimeout: 10000,
-  },
-  config: {
-    lists: [
-      {
-        link: {
-          url: 'http://freeproxylists.net/',
-        },
-        items: [
-          {
-            selector: 'table.DataGrid tbody tr:not(:first-child)',
-            attributes: [
-              {
-                name: 'ipAddress',
-                selector: 'td:nth-child(1) a',
-                parse: function (text) {
-                  return text.trim() || null;
-                },
-              },
-              {
-                name: 'port',
-                selector: 'td:nth-child(2)',
-                parse: function (text) {
-                  var port = parseInt(text.trim());
-                  if (_.isNaN(port)) return null;
-                  return port;
-                },
-              },
-              {
-                name: 'protocols',
-                selector: 'td:nth-child(3)',
-                parse: function (text) {
-                  if (!text) return null;
-                  text = text.trim().toLowerCase();
-                  var protocol = convert.protocols[text] || 'http';
-                  return [protocol];
-                },
-              },
-              {
-                name: 'anonymityLevel',
-                selector: 'td:nth-child(7)',
-                parse: function (text) {
-                  if (!text) return null;
-                  text = text.trim().toLowerCase();
-                  return convert.anonymityLevels[text] || null;
-                },
-              },
-            ],
-          },
-        ],
-        pagination: {
-          next: {
-            selector: 'div.page a:last-child',
-          },
-        },
-      },
-    ],
-  },
+	homeUrl: 'http://freeproxylists.net/',
+	abstract: 'list-crawler',
+	defaultOptions: {
+		defaultTimeout: 10000,
+	},
+	config: {
+		lists: [
+			{
+				link: {
+					url: 'http://freeproxylists.net/',
+				},
+				items: [
+					{
+						selector: 'table.DataGrid tbody tr:not(:first-child)',
+						attributes: [
+							{
+								name: 'ipAddress',
+								selector: 'td:nth-child(1) a',
+								parse: function (text) {
+									return text.trim() || null;
+								},
+							},
+							{
+								name: 'port',
+								selector: 'td:nth-child(2)',
+								parse: function (text) {
+									var port = parseInt(text.trim());
+									if (_.isNaN(port)) return null;
+									return port;
+								},
+							},
+							{
+								name: 'protocols',
+								selector: 'td:nth-child(3)',
+								parse: function (text) {
+									if (!text) return null;
+									text = text.trim().toLowerCase();
+									var protocol = convert.protocols[text] || 'http';
+									return [protocol];
+								},
+							},
+							{
+								name: 'anonymityLevel',
+								selector: 'td:nth-child(7)',
+								parse: function (text) {
+									if (!text) return null;
+									text = text.trim().toLowerCase();
+									return convert.anonymityLevels[text] || null;
+								},
+							},
+						],
+					},
+				],
+				pagination: {
+					next: {
+						selector: 'div.page a:last-child',
+					},
+				},
+			},
+		],
+	},
 };
