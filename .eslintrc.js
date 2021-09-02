cdc175b742a776e1f2f280213e8bc00151ad2941
Charles Hill <chill@degreesofzero.com>
2021-09-02 16:35:42 +0100

    Upgraded deps

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	'extends': 'eslint:recommended',
-	'rules': {
-		'indent': ['error', 'tab', { 'SwitchCase': 1 }],
-		'no-console': 0,
-		'no-undef': 0,
-		'no-unused-vars': ['error', { 'vars': 'all', 'args': 'none' }]
-	}
-};
